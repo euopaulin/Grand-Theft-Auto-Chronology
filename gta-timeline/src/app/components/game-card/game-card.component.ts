882055a700846eee2c4b1a241711e02308237ea7
Paulo Henrique <157616381+euopaulin@users.noreply.github.com>
2025-05-19 12:41:14 -0300

    Update

--- gta-timeline/src/app/components/game-card/game-card.component.ts
+++ gta-timeline/src/app/components/game-card/game-card.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-game-card',
   templateUrl: './game-card.component.html',
   styleUrls: ['./game-card.component.scss']
 })
 export class GameCardComponent {
-
+@Input() title!:string;
+@Input() description!:string;
+@Input() image!:string;
+@Input() releaseDate!:string;
+@Input() platforms!:string[];
 }
--- gta-timeline/src/app/pages/home/home.component.ts
+++ gta-timeline/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {games} from 'src/assets/Data/games';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  games = games;
 
 }
